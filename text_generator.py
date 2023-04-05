cc6846ddc2868282c5ad7f948aab1be10912d475
unknown <lawrence.lee784@gmail.com>
2023-04-05 14:51:28 +0100

    Finish building model

--- text_generator.py
+++ text_generator.py
@@ -7,18 +7,23 @@ import os
 import time
 
 
-directory = os.getcwd()
-path_to_file = tf.keras.utils.get_file('text_data', "file:\\" + directory + "\\text_data.txt")
-text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
-sentences = pd.read_csv("all_sentences")
+#directory = os.getcwd()
+#path_to_file = tf.keras.utils.get_file('text_data', "file:\\" + directory + "\\text_data.txt")
+#text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 
-sentences = sentences.astype(str).values.flatten().tolist()
+def get_data():
+    sentences = pd.read_csv("all_sentences")
+    sentences = sentences.astype(str).values.flatten().tolist()
+    return sentences
 
-
-def get_training_sequences(sentences):
+def tokenize(sentences):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(sentences)
     tokenized_sentences = tokenizer.texts_to_sequences(sentences)
+    num_words = len(tokenizer.word_index)
+    return tokenized_sentences, num_words
+
+def get_training_sequences(tokenized_sentences):
     training_sequences=[]
     for tokenized_sentence in tokenized_sentences:
         for word_number in range(1, len(tokenized_sentence)):
@@ -29,9 +34,27 @@ def get_training_sequences(sentences):
 
 def get_padded_sequences(training_sequences):
     max_length = max([len(x) for x in training_sequences])
-    padded_sequences = pad_sequences(training_sequences, padding='post', truncating='post', maxlen=max_length)
-    return padded_sequences
+    padded_sequences = np.array(pad_sequences(training_sequences, padding='pre', maxlen=max_length))
+    return padded_sequences, max_length
 
-#input_sequences = np.array(train_padded)
 
-print(get_padded_sequences(get_training_sequences(sentences)[:1]))
+def get_training_data(padded_sequences):
+    predictors = padded_sequences[:, :-1]
+    targets = padded_sequences[:, -1]
+    return predictors, targets
+
+
+def build_model(num_words, max_length):
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Embedding(num_words+1, 10, input_length=max_length-1))
+    model.add(tf.keras.layers.LSTM(128))
+    model.add(tf.keras.layers.Dense(num_words+1, activation='softmax'))
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
+    return model
+
+tokenized_sentences, num_words = tokenize(get_data())
+padded_sequences, max_length = get_padded_sequences(get_training_sequences(tokenized_sentences))
+predictors, targets = get_training_data(padded_sequences)
+model = build_model(num_words, max_length)
+print(model.summary())
+model.fit(predictors, targets, epochs=20)
